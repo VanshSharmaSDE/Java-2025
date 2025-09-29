88d62f47e8e5bf6613ff29f79105e71e845c9e92
VanshSharmaSDE <vansh4664@gmail.com>
2025-09-29 20:21:06 +0530

    Refactor Binary Search implementation to include start and end parameters for improved recursion

--- September/BinarySerchRecusrion.java
+++ September/BinarySerchRecusrion.java
@@ -1,9 +1,24 @@
 public class BinarySerchRecusrion{
     public static void main(String[] args) {
-        
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        System.out.println(BS(arr,9,0,arr.length - 1));
     }
 
-    static int BS(int[] arr , int target){
-        
+    static int BS(int[] arr , int target , int start , int end){
+       if(start > end){
+         return -1;
+       }
+
+       int mid = start + (end - start) / 2;
+
+       if(arr[mid] == target){
+         return mid;
+       }
+
+       if(target > mid){
+        return BS(arr , target , mid + 1 , end);
+       }
+
+       return BS(arr , target , start , mid - 1);
     }
 }
